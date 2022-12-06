88e835ee3b078a7fbc5ab8269a0d6fae7cd1c81f
Tanmaya123-t <tanmayakumar50@gmail.com>
2022-12-06 09:26:56 +0530

    added add method in sumOfTwoNumber.java class

---
+++ SampleDemo-GIT/src/com/add/SumOfTwoNumber.java
@@ -0,0 +1,12 @@
+package com.add;
+
+public class SumOfTwoNumber {
+	
+	int a=10;
+	int b=20;
+	
+	public int add() {
+		return a+b;
+	}
+
+}
--- SampleDemo-GIT/src/com/nt/Test.java
+++ SampleDemo-GIT/src/com/nt/Test.java
@@ -1,9 +1,15 @@
 package com.nt;
 
+import com.add.SumOfTwoNumber;
+
 public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("main executed");
+		
+		SumOfTwoNumber sum = new SumOfTwoNumber();
+		int result=sum.add();
+		System.out.println(result);
 	}
 
 }
